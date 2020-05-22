ee84026d74bcba847278ec5653d99a6b8ab47cf0
samuelgamil <samuelgamil72@gmail.com>
2020-05-22 16:49:27 -0700

    creating profile page

--- lib/AuthenticationPackage/ProfilePage.dart
+++ lib/AuthenticationPackage/ProfilePage.dart
@@ -1,144 +1,192 @@
 import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:conanschool/HomePackage/HomePage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/widgets.dart';
 import 'package:path/path.dart' as path;
 
 class ProfilePage extends StatefulWidget {
-  File file;
-
   @override
   _ProfilePageState createState() => _ProfilePageState();
 }
 
 class _ProfilePageState extends State<ProfilePage> {
-  String _email, _password;
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  // get uid
+  Future<String> getCurrentUid() async {
+    return (await _firebaseAuth.currentUser()).uid;
+  }
+
+  // get current user
+  Future getCurrentUser() async {
+    return await _firebaseAuth.currentUser();
+  }
+
+  //get dataUser
+
   bool readOnly = true;
 
   File _image;
+  String fileName;
+
+   Future getImage() async{
+     var selectedImage=await ImagePicker.pickImage(source: ImageSource.gallery);
+     setState(() {
+       _image=selectedImage;
+       fileName=path.basename(_image.path);
+     });
+
+   }
+
+
+   Future <String>uploadPic()async{
+     StorageReference ref=FirebaseStorage.instance.ref().child(fileName);
+     StorageUploadTask uploadTask=ref.putFile(_image);
+     var downloadUrl = await(await uploadTask.onComplete ).ref.getDownloadURL();
+      var url=downloadUrl.toString();
+     print("download url $url");
+     setState(() {
+       Scaffold.of(context)
+           .showSnackBar(SnackBar(content: Text('Profile Picture Uploaded')));
+     });
+
+     return url;
+   }
+
+
+ 
+
+ 
+
+  TextEditingController controllerName = TextEditingController();
+  TextEditingController controllerEmail = TextEditingController();
+  TextEditingController controllerPassword = TextEditingController();
+  TextEditingController controllerPhone = TextEditingController();
+  TextEditingController controllerAge = TextEditingController();
+
+  updateUserInfo(String ID) {
+    Firestore.instance.collection('Profile').document(ID).updateData({
+      'name':controllerName.text,
+      'email':controllerEmail.text,
+      'password':controllerPassword.text,
+      'phone':controllerPhone.text,
+      'age':controllerAge.text,
+      'imageUrl':uploadPic(),
+    }).then((value) {
+      print('record updated successflly');
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    Future getImage() async {
-      var image = await ImagePicker.pickImage(source: ImageSource.gallery);
-
-      setState(() {
-        _image = image;
-        //print('Image Path $_image');
-      });
-    }
-
-    //for upload image in firebaase
-    FirebaseStorage _storage =
-        FirebaseStorage(storageBucket: 'gs://conan-school.appspot.com');
-
-    Future uploadPic(BuildContext context) async {
-      String fileName = path.basename(_image.path);
-      StorageReference firebaseStorageRef =
-          FirebaseStorage.instance.ref().child(fileName);
-      StorageUploadTask uploadTask = firebaseStorageRef.putFile(_image);
-      StorageTaskSnapshot taskSnapshot = await uploadTask.onComplete;
-      setState(() {
-        Scaffold.of(context).showSnackBar(
-            SnackBar(content: Text('Profile Picture is Updated')));
-      });
-    }
-
     return Scaffold(
-      body: Builder(
-        builder: (context) => Container(
-            child: ListView(
-          children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: <Widget>[
-                SizedBox(
-                  height: 20.0,
-                ),
-                Align(
-                  alignment: Alignment.center,
-                  child: CircleAvatar(
-                    radius: 80,
-                    backgroundColor: Color(0xff476cfb),
-                    child: ClipOval(
-                      child: new SizedBox(
-                        width: 140.0,
-                        height: 140.0,
-                        child: (_image != null)
-                            ? Image.file(
-                                _image,
-                                fit: BoxFit.fill,
-                              )
-                            : Image.network(
-                                "$_image",
-                                fit: BoxFit.fill,
-                              ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('Profile').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return Text('Loading');
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return new Text('Loading...');
+            default:
+              return new ListView(
+                children:
+                    snapshot.data.documents.map((DocumentSnapshot document) {
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      _sizedbox(20.0),
+                      Align(
+                        alignment: Alignment.center,
+                        child: CircleAvatar(
+                          radius: 80,
+                          backgroundColor: Color(0xff476cfb),
+                          child: ClipOval(
+                            child: new SizedBox(
+                              width: 140.0,
+                              height: 140.0,
+                              child: (_image!= null)
+                                  ? Image.file(
+                                      _image,
+                                      fit: BoxFit.fill,
+                                    )
+                                  : Image.network(
+                                      document['imageUrl'],fit: BoxFit.fill,
+                                    ),
+                            ),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: IconButton(
-                    icon: Icon(
-                      FontAwesomeIcons.camera,
-                      size: 30.0,
-                    ),
-                    onPressed: () {
-                      getImage();
-                    },
-                  ),
-                ),
-              ],
-            ),
-            _dataUser("User Name", "Samuel Gamil", 10.0),
-            _dataUser("E-mail", "samuelgamil693@gmail.com", 10.0),
-            _dataUser("Password", "vrbrfbrb", 10.0),
-            _dataUser("Phone Number", "+201211840139", 10.0),
-            _dataUser("Age", "21", 10.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                RaisedButton(
-                  color: Color(0xff476cfb),
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  elevation: 4.0,
-                  splashColor: Colors.blueGrey,
-                  child: Text(
-                    'Cancel',
-                    style: TextStyle(color: Colors.white, fontSize: 16.0),
-                  ),
-                ),
-                RaisedButton(
-                  color: Color(0xff476cfb),
-                  onPressed: () {
-                    uploadPic(context);
-                  },
-                  elevation: 4.0,
-                  splashColor: Colors.blueGrey,
-                  child: Text(
-                    'Submit',
-                    style: TextStyle(color: Colors.white, fontSize: 16.0),
-                  ),
-                ),
-              ],
-            ),
-            _sizedbox(15.0),
-          ],
-        )),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                        child: IconButton(
+                          icon: Icon(
+                            FontAwesomeIcons.camera,
+                            size: 30.0,
+                          ),
+                          onPressed: () {
+                            getImage();
+                          },
+                        ),
+                      ),
+                      _dataUser("User Name", "  ${document['name']} ", 10.0,
+                          controllerName),
+                      _dataUser(
+                          "E-mail", document['email'], 10.0, controllerEmail),
+                      _dataUser("Password", document['password'], 10.0,
+                          controllerPassword),
+                      _dataUser("Phone Number", document['phone'], 10.0,
+                          controllerPhone),
+                      _dataUser("Age", document['age'], 10.0, controllerAge),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          RaisedButton(
+                            color: Color(0xff476cfb),
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            elevation: 4.0,
+                            splashColor: Colors.blueGrey,
+                            child: Text(
+                              'Cancel',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 16.0),
+                            ),
+                          ),
+                          RaisedButton(
+                            color: Color(0xff476cfb),
+                            onPressed: () {
+                              uploadPic;
+                              updateUserInfo(getCurrentUid().toString());
+                            },
+                            elevation: 4.0,
+                            splashColor: Colors.blueGrey,
+                            child: Text(
+                              'Submit',
+                              style: TextStyle(
+                                  color: Colors.white, fontSize: 16.0),
+                            ),
+                          ),
+                        ],
+                      ),
+                      _sizedbox(20.0),
+                    ],
+                  );
+                }).toList(),
+              );
+          }
+        },
       ),
     );
   }
 
-  Widget _dataUser(String title, String text, double _left) {
+  Widget _dataUser(
+      String title, String text, double _left, TextEditingController _text) {
+    final _formkey = GlobalKey<FormState>();
     return Container(
       padding: EdgeInsets.only(left: 10.0),
       child: Row(
@@ -158,7 +206,9 @@ class _ProfilePageState extends State<ProfilePage> {
             width: MediaQuery.of(context).size.width - 110,
             padding: EdgeInsets.only(top: 20, left: _left),
             child: Form(
+              key: _formkey,
               child: TextFormField(
+                controller: controllerName,
                 autofocus: true,
                 readOnly: readOnly,
                 decoration: InputDecoration(
@@ -186,8 +236,6 @@ class _ProfilePageState extends State<ProfilePage> {
                 maxLines: 1,
                 textAlign: TextAlign.left,
                 textInputAction: TextInputAction.done,
-                validator: (val) {},
-                onSaved: (val) {},
               ),
             ),
           ),
